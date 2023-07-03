c1681c61e9dd50b6ccc7bcbb400b367040e38e0e
Orca <1556962921@qq.com>
2023-07-03 19:14:55 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 22535766
+API_HASH = "ebb6fec4dcf17596adfb0c04a05d20ea"
+BOT_TOKEN = "6193291147:AAFvKyTuWMWtw-n5HQvwh71oXifXPXMCy5I"
+SESSION = "AQFX3lYAKFCTiSaGq6CmFKVhbwCIB2aBwffgqzYMy1VudIgoIjrM-Ne_vodUtCbLcDu4vPzk8qX_J7RJbVY9W9e0c6LIPFvNIm064XygolrWULf4kfERq1eJxHMQ2HFls704pOcsLdQh6gcoQYrhgPPnGxHbavIdX_Gya6w08IJ7gjsMEXjBe4_bc13VZeTm1YCT5IXapnrb8bqm0ROG45wBP9UCNFxm8MhpR6eShgw7VdWVPy4RZQAa2S43mKYxSXlcJ51OS5aEBvBewfZn7c7CoSjKf72Y2DEbk6xOqyQqd-pA__-S8-nITNEGC4hnZ3CCBCjdBKp8Ueo16x2DNVBc2HjQlQAAAAFiZi0aAA"
+FORCESUB = "SaveRSC"
+AUTH = "5945830682"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
